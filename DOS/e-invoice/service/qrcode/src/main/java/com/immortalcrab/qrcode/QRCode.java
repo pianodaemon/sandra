fc4d9539e37150056399e8bc53cd8f9108148d5c
Edwin Plauchu <pianodaemon@gmail.com>
2022-01-11 16:40:32 -0600

    Took needless code out from qrcode component

--- DOS/e-invoice/service/qrcode/src/main/java/com/immortalcrab/qrcode/QRCode.java
+++ DOS/e-invoice/service/qrcode/src/main/java/com/immortalcrab/qrcode/QRCode.java
@@ -14,23 +14,11 @@ import java.nio.file.Path;
 
 public class QRCode {
 
-    public static void main(String[] args) {
-        try {
-            var arr = new String[] {"34598foijsdof89uj34oij", "oisdf8u34okjoisjdfdf", "wepokczmn7823"};
-            for (String i : arr) {
-                generate(i, 1250, 1250, "/home/userd/qrcode_" + i + ".png");
-            }
-        } catch (Exception e) {
-            System.out.println("Could not generate QR Code" + e);
-        }
-    }
-
     public static void generate(String text, int width, int height, String filePath) throws Exception {
         QRCodeWriter qcWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qcWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
         Path path = FileSystems.getDefault().getPath(filePath);
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-        // MatrixToImageWriter.writeToPath(bitMatrix, "JPEG", path);
     }
 
     public static ByteArrayInputStream generateByteStream(String text, int width, int height) throws Exception {
