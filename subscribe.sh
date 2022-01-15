a1745f1cccd9fd5df5f9144e5d2769ce989506b3
Edwin Plauchu <pianodaemon@gmail.com>
2022-01-15 11:44:18 -0600

    Minimalistic script to deploy the subscriptor's stack

---
+++ subscribe.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+# XXX: Before run this
+# Don't forget to set AWS_PROFILE with the respective entitled profile
+
+# Deploys the subscriptor stack
+__deploy_stack() {
+
+	local temp="subscriptor_stack.yaml"
+	local deploy_cmd=$(printf 'aws cloudformation deploy --stack-name %s --template-file %s' "${1}" "${temp}")
+
+	# Verification of presence
+	if [[ ! -f $temp ]]; then
+		echo "Cloudformation template not found"
+		exit 1
+	fi
+
+	# Verification of content
+	if [[ ! -s $temp ]]; then
+		echo "Emptyness at Cloudformation template's content"
+		exit 1
+	fi
+
+	$deploy_cmd
+}
+
+__deploy_stack $1
