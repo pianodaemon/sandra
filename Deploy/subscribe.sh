a90b4521a16e3fd25e17f2a126894e9e7b9b1f50
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-01-16 09:21:46 -0600

    Reformated with shfmt
    * shfmt -ci -s -w subscribe.sh
    
    Signed-off-by: Edwin Pĺauchu <pianodaemon@gmail.com>

--- Deploy/subscribe.sh
+++ Deploy/subscribe.sh
@@ -6,22 +6,22 @@
 # Deploys the subscriptor stack
 __deploy_stack() {
 
-        local temp="subscriptor_stack.yaml"
-        local deploy_cmd=$(printf 'aws cloudformation deploy --stack-name %s --template-file %s  --capabilities CAPABILITY_NAMED_IAM' "${1}" "${temp}")
+	local temp="subscriptor_stack.yaml"
+	local deploy_cmd=$(printf 'aws cloudformation deploy --stack-name %s --template-file %s  --capabilities CAPABILITY_NAMED_IAM' "${1}" "${temp}")
 
-        # Verification of presence
-        if [[ ! -f $temp ]]; then
-                echo "Cloudformation template not found"
-                exit 1
-        fi
+	# Verification of presence
+	if [[ ! -f $temp ]]; then
+		echo "Cloudformation template not found"
+		exit 1
+	fi
 
-        # Verification of content
-        if [[ ! -s $temp ]]; then
-                echo "Emptyness at Cloudformation template's content"
-                exit 1
-        fi
+	# Verification of content
+	if [[ ! -s $temp ]]; then
+		echo "Emptyness at Cloudformation template's content"
+		exit 1
+	fi
 
-        $deploy_cmd
+	$deploy_cmd
 }
 
 __deploy_stack $1
