7084d93f5125e27aa74e8041702b7cc2fabc2540
Edwin Plauchu <pianodaemon@gmail.com>
2023-10-23 18:41:45 -0600

    Create BillOcr.java

---
+++ DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/ocr/BillOcr.java
@@ -0,0 +1,68 @@
+package com.immortalcrab.bill.ocr;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+@AllArgsConstructor
+public class BillOcr {
+
+    private static final int PAGE_DOES_NOT_MATTER = 0;
+
+    protected final @NonNull
+    ITransformer imgTransformer;
+
+    protected final @NonNull
+    IBillDistributionProvider distProvider;
+
+    @FunctionalInterface
+    public interface ITransformer {
+
+        public String[] transformFromFile(final String outputImageDirectory, File file) throws IOException;
+    }
+
+    @FunctionalInterface
+    public interface IBillDistributionProvider {
+
+        public BillDistribution obtainFromFile(String distPath) throws IOException;
+    }
+
+    private ITesseract setupTesseract() {
+        ITesseract tesseract = new Tesseract();
+        tesseract.setDatapath("tessdata");
+        tesseract.setPageSegMode(11);
+        tesseract.setOcrEngineMode(2);
+
+        File directory = new File("tessdata");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        return tesseract;
+    }
+
+    public Map<String, Object> fetchSymbols(String pdfFilePath, String distPath) throws IOException, TesseractException {
+        Map<String, Object> syms = new HashMap<>();
+        ITesseract tesseract = setupTesseract();
+        BillDistribution dist = distProvider.obtainFromFile(distPath);
+        String[] paths = imgTransformer.transformFromFile(null, new File(pdfFilePath));
+        for (int idx = 0; idx < paths.length; idx++) {
+            final int pageNumber = idx + 1;
+            for (BillDistribution.Distribution d : dist.getDistributions()) {
+                if ((d.getPage() == PAGE_DOES_NOT_MATTER) || (d.getPage() == pageNumber)) {
+                    for (var section : d.getSections()) {
+                        final String ocrResult = tesseract.doOCR(new File(paths[idx]), section.getRect());
+                        syms.put(section.getTitle(), ocrResult);
+                    }
+                }
+            }
+        }
+        return syms;
+    }
+}
