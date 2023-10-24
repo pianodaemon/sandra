61e95ec5311862ee0b53fa6051d7482fe4c35d30
Edwin Plauchu <pianodaemon@gmail.com>
2023-10-23 18:44:20 -0600

    Update OcrHelper.java

--- DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/ocr/OcrHelper.java
+++ DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/ocr/OcrHelper.java
@@ -1,10 +1,10 @@
 package com.immortalcrab.bill.ocr;
 
 import com.immortalcrab.bill.pdf.RenderPngHelper;
-import java.io.File;
 import java.io.IOException;
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.sourceforge.tess4j.TesseractException;
 
 public class OcrHelper {
@@ -13,48 +13,20 @@ public class OcrHelper {
 
     public static void main(String[] args) {
 
-        String pdfFilePath = "C:/NOMINATOR/908158-PrecioVenta.pdf";
-
         try {
-
-            BillDistribution dist = BillDistribution.obtainFromFile("distribution.json");
-            String[] paths = RenderPngHelper.transformFromPdf(null, new File(pdfFilePath));
-            for (int idx = 0; idx < paths.length; idx++) {
-                extract(dist, paths[idx], idx + 1);
+            var bocr = new BillOcr(RenderPngHelper::transformFromPdf, BillDistribution::obtainFromFile);
+            String pdfFilePath = "C:/NOMINATOR/908158-PrecioVenta.pdf";
+            Map<String, Object> syms = bocr.fetchSymbols(pdfFilePath, "distribution.json");
+            for (String name : syms.keySet()) {
+                String key = name.toString();
+                String value = syms.get(name).toString();
+                System.out.println(key + " " + value);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private static void printSection(ITesseract tesseract, BillDistribution.Distribution dist, final String imagePath) throws TesseractException {
-        for (var section : dist.getSections()) {
-            String ocrResult = tesseract.doOCR(new File(imagePath), section.getRect());
-            System.out.println("Section " + section.getTitle() + " Text:\n" + ocrResult);
-        }
-    }
-
-    private static void extract(BillDistribution dist, final String imagePath, final int pageNumber) {
-
-        // Set up the Tesseract instance
-        ITesseract tesseract = new Tesseract();
-        File directory = new File("tessdata");
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
-        tesseract.setDatapath("tessdata"); // Set the path to your Tesseract data directory
-
-        try {
-            // Perform OCR on the image
-            tesseract.setPageSegMode(11);
-            tesseract.setOcrEngineMode(2);
-            for (BillDistribution.Distribution d : dist.getDistributions()) {
-                if ((d.getPage() == PAGE_DOES_NOT_MATTER) || (d.getPage() == pageNumber)) {
-                    printSection(tesseract, d, imagePath);
-                }
-            }
-        } catch (TesseractException e) {
-            e.printStackTrace();
+        } catch (TesseractException ex) {
+            Logger.getLogger(OcrHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
