23e8c9a7a720bf2ff4f0c96e7075bc642cac2a3f
Edwin Plauchu <j4nusx@yahoo.com>
2023-10-27 08:14:46 -0500

    xml_file_output

--- DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/struct/XmlWritingHelper.java
+++ DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/struct/XmlWritingHelper.java
@@ -2,6 +2,7 @@ package com.immortalcrab.bill.struct;
 
 import com.immortalcrab.bill.ocr.InvoiceOcrException;
 import org.w3c.dom.Document;
+import java.io.File;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
@@ -9,18 +10,13 @@ import java.io.StringWriter;
 
 public class XmlWritingHelper {
 
-    public static StringWriter indentateDocument(final Document doc) throws InvoiceOcrException {
+    public static void writeXMLToFile(String filePath, Document document) throws InvoiceOcrException {
         try {
-            TransformerFactory transformerFactory = javax.xml.transform.TransformerFactory.newInstance();
-            Transformer transformer;
-            transformer = transformerFactory.newTransformer();
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
             transformer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            DOMSource source = new javax.xml.transform.dom.DOMSource(doc);
-            StringWriter writer = new java.io.StringWriter();
-            StreamResult result = new javax.xml.transform.stream.StreamResult(writer);
-            transformer.transform(source, result);
-            return writer;
+            transformer.transform(new DOMSource(document), new StreamResult(new File(filePath)));
         } catch (TransformerException ex) {
             throw new InvoiceOcrException("", ex);
         }
