4c30cb0e5a96ede0b46bcfb26b353acf4deaa5d2
Edwin Plauchu <pianodaemon@gmail.com>
2023-10-29 14:20:11 -0600

    Update IOutputFormater.java

--- DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/struct/IOutputFormater.java
+++ DOS/ETL/elemental/src/main/java/com/immortalcrab/bill/struct/IOutputFormater.java
@@ -5,4 +5,10 @@ import com.immortalcrab.bill.ocr.InvoiceOcrException;
 public interface IOutputFormater<W> {
 
     public W render() throws InvoiceOcrException;
+
+    public void saveOnStorage(String filePath, W w) throws InvoiceOcrException;
+
+    public default void renderFeaturingSave(String xmlFilePath) throws InvoiceOcrException {
+        saveOnStorage(xmlFilePath, render());
+    }
 }
